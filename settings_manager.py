26605b56a08033e6123a03cf9b832ea1b32b0769
ArtemKalchenkoGit <artem.kalchenko2004@gmail.com>
2026-03-25 00:08:33 +0200

    getting ready for deploy

--- settings_manager.py
+++ settings_manager.py
@@ -4,13 +4,13 @@ _settings = {}
 
 def load_settings():
     global _settings
-    with open("settings.json",encoding="utf-8") as f:
+    with open("data/settings.json", encoding="utf-8") as f:
         _settings = json.load(f)
 
 def save(key: str, value):
     global _settings
     _settings[key] = value
-    with open("settings.json", "w", encoding="utf-8") as f:
+    with open("data/settings.json", "w", encoding="utf-8") as f:
         json.dump(_settings, f, ensure_ascii=False, indent=2)
 
 def get(key: str, default=None):
